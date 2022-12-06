9072920675c718d5653352f9ba27f75030ff0f90
anatawa12 <anatawa12@icloud.com>
2022-12-06 12:17:12 +0900

    wip: simplify EnumToFloatHelper

--- Generator/Utils.cs
+++ Generator/Utils.cs
@@ -12,35 +12,31 @@ namespace Anatawa12.AnimatorControllerAsACode.Generator
             AssetDatabase.AddObjectToAsset(obj, file);
         }
 
-        public static Func<float, float> FloatToFloat = x => x;
-        public static Func<int, float> IntToFloat = x => x;
-        public static Func<bool, float> BoolToFloat = x => x ? 1f : 0f;
+        public static readonly Func<float, float> FloatToFloat = x => x;
+        public static readonly Func<int, float> IntToFloat = x => x;
+        public static readonly Func<bool, float> BoolToFloat = x => x ? 1f : 0f;
 
+        // Animation only supports int-based enums.
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Func<T, float> EnumToFloat<T>()
             where T : unmanaged, Enum
-            => EnumToFloatHelper<T>.ToFloat;
+            => EnumToFloatHelper<T>.ToFloat ?? ThrowEnumUnderlyingTypeError<T>();
+
+        private static Func<T, float> ThrowEnumUnderlyingTypeError<T>() =>
+            throw new ArgumentException("UnderlyingType of T is not int. Animating such a field is not supported.",
+                nameof(T));
 
         private static class EnumToFloatHelper<T>
             where T : unmanaged, Enum
         {
-            public static Func<T, float> ToFloat = Create();
+            public static readonly Func<T, float> ToFloat = Create();
 
             private static Func<T, float> Create()
             {
-                switch (Unsafe.SizeOf<T>())
-                {
-                    case 1: return ForOneByte;
-                    case 2: return ForTwoByte;
-                    case 4: return ForFourByte;
-                    case 8: return ForEightByte;
-                    default: throw new ArgumentException("sizeof(T) is not 1, 2, 4, nor 8", nameof(T));
-                }
+                if (Enum.GetUnderlyingType(typeof(T)) == typeof(int))
+                    return t => Unsafe.As<T, int>(ref t);
+                return null;
             }
-
-            private static float ForOneByte(T t) => Unsafe.As<T, byte>(ref t);
-            private static float ForTwoByte(T t) => Unsafe.As<T, short>(ref t);
-            private static float ForFourByte(T t) => Unsafe.As<T, int>(ref t);
-            private static float ForEightByte(T t) => Unsafe.As<T, long>(ref t);
         }
     }
 }
