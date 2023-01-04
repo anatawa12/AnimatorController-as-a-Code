4e8f6055bb2705047aa6b43485e320db4f7d0ee1
anatawa12 <anatawa12@icloud.com>
2023-01-04 21:31:46 +0900

    fix: transition default values

--- Framework/AccTransition.cs
+++ Framework/AccTransition.cs
@@ -27,6 +27,14 @@ namespace Anatawa12.AnimatorControllerAsACode.Framework
     {
         internal AccTransition(AnimatorStateTransition transition, AccStateMachineBase stateMachine) : base(transition, stateMachine)
         {
+            transition.duration = 0;
+            transition.hasExitTime = false;
+            transition.exitTime = 0;
+            transition.hasFixedDuration = true;
+            transition.offset = 0;
+            transition.interruptionSource = TransitionInterruptionSource.None;
+            transition.orderedInterruption = true;
+            transition.canTransitionToSelf = false;
         }
 
         public AccTransition WithTransitionDurationSeconds(float seconds)
