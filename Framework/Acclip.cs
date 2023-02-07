79209b9935c58bc622a414f0d5aa3da1a89498c6
anatawa12 <anatawa12@icloud.com>
2023-02-07 15:34:40 +0900

    fix: remove AnimatesAnimator which is invalid

--- Framework/Acclip.cs
+++ Framework/Acclip.cs
@@ -50,20 +50,6 @@ namespace Anatawa12.AnimatorControllerAsACode.Framework
         public BoolSettingCurve Animates(GameObject gameObject) =>
             AnimatesBool(gameObject.transform, typeof(GameObject), "m_IsActive");
 
-        private EditorCurveBinding AnimatorBinding<T>(AccParameter<T> floatParameter) => new EditorCurveBinding
-        {
-            path = "",
-            type = typeof(Animator),
-            propertyName = floatParameter.Name
-        };
-
-        public BoolSettingCurve AnimatesAnimator(AccParameter<bool> parameter) => new BoolSettingCurve(_clip, AnimatorBinding(parameter));
-        public FloatSettingCurve AnimatesAnimator(AccParameter<float> parameter) => new FloatSettingCurve(_clip, AnimatorBinding(parameter));
-        public IntSettingCurve AnimatesAnimator(AccParameter<int> parameter) => new IntSettingCurve(_clip, AnimatorBinding(parameter));
-        public EnumSettingCurve<T> AnimatesAnimator<T>(AccParameter<T> parameter)
-            where T : Enum
-            => new EnumSettingCurve<T>(_clip, AnimatorBinding(parameter));
-
         public EditorCurveBinding BindingFromComponent(Component anyComponent, string propertyName) => 
             _config.Binding(anyComponent.transform, anyComponent.GetType(), propertyName);
     }
